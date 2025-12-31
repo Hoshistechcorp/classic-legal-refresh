8e84dcf3925c2405981cbf85cc6385a1c9b70da5
theijhay <olawaleisaacjohn@gmail.com>
2025-12-31 17:40:29 +0100

    Add 404 redirect page, update deployment script, and configure gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index"; 
 import About from "./pages/About";
 import PracticeAreas from "./pages/PracticeAreas";
 import Reviews from "./pages/Reviews";
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
@@ -29,7 +29,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </TooltipProvider>
   </QueryClientProvider>
 );
